8fd5459937ed2c54b665f488e90ccca3b35e977d
ARKaraoglu <ahmetrusenkara@gmail.com>
2024-09-23 23:19:05 -0400

    Modified main.py, player.ph
    Using pygame groups and containers for drawing and updating player

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     dt = 0
+
+    updatableGroup = pygame.sprite.Group()
+    drawableGroup = pygame.sprite.Group()
+    Player.containers = (updatableGroup, drawableGroup) #type: ignore 
   
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
-
-        player.update(dt)
+        
+        for obj in updatableGroup:
+            obj.update(dt) 
         screen.fill((0,0,0))
-        player.draw(screen)
-        pygame.display.flip()
+        for sprite in drawableGroup:
+            sprite.draw(screen)
+        
    
         dt = clock.tick(60) / 1000 
+        
+        pygame.display.flip()
             
 if __name__ == "__main__":
     main()
