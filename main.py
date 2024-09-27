8cd0605436c042ac5d4f7fd356ba622ebd2d00ab
ARKaraoglu <ahmetrusenkara@gmail.com>
2024-09-27 19:15:01 -0400

    Modified constants.py, main.py, player.py
    Able to shoot bullets and implemented collision between asteroids and bullets

--- main.py
+++ main.py
@@ -43,6 +43,10 @@ def main():
             if asteroid.collision(player) is True:
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill((0,0,0))
         for sprite in drawableGroup:
--- player.py
+++ player.py
@@ -7,7 +7,8 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-    
+        self.shootTimer = 0
+
     def draw(self, screen):
        pygame.draw.polygon(surface = screen, color = "white", points = self.triangle(), width = 2) 
 
@@ -24,6 +25,7 @@ class Player(CircleShape):
         self.rotation += dt * PLAYER_TURN_SPEED
 
     def update(self, dt):
+        self.shootTimer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -42,9 +44,11 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shootTimer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-
+        self.shootTimer = PLAYER_SHOOT_COOLDOWN
 
 
 
